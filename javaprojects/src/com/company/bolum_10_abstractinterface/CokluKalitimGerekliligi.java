2d054bbfb3e3838730bb59cd472bf7fe5fc70354
muhammedazman <vmazman@gmail.com>
2021-02-17 21:20:22 +0100

    Interface konusuna giris yapildi ve ornek kod yazildi

---
+++ javaprojects/src/com/company/bolum_10_abstractinterface/CokluKalitimGerekliligi.java
@@ -0,0 +1,122 @@
+package com.company.bolum_10_abstractinterface;
+
+import java.util.ArrayList;
+
+public class CokluKalitimGerekliligi {
+
+    public static void main(String[] args) {
+
+        MuslumBaba muslum = new MuslumBaba();
+        muslum.sahnedeSigaraIc();
+
+        PopSarkiciOzellikleri popSarkici1 = new Tarkan();
+        PopSarkiciOzellikleri popSarkici2 = new Hadise();
+
+        PopSarkiciOzellikleri dizi[] = new PopSarkiciOzellikleri[5];
+        dizi[0] = popSarkici1;
+        dizi[1] = popSarkici2;
+
+        ArrayList<PopSarkiciOzellikleri> sarkicilar = new ArrayList<>();
+        sarkicilar.add(popSarkici1);
+        sarkicilar.add(popSarkici2);
+    }
+}
+
+interface Kisi{
+    void yemekYe();
+    void sporYap();
+}
+
+abstract class Sarkici implements Kisi {
+    abstract void sarkiSoyle();
+}
+
+interface ArabeskSarkiciOzellikleri {
+    void sahnedeSigaraIc();
+}
+
+interface PopSarkiciOzellikleri {
+    void sahnedeDansEt();
+    void duetYap();
+}
+
+class Tarkan implements PopSarkiciOzellikleri{
+
+    @Override
+    public void sahnedeDansEt() {
+
+    }
+
+    @Override
+    public void duetYap() {
+
+    }
+}
+
+class Hadise implements PopSarkiciOzellikleri{
+
+    @Override
+    public void sahnedeDansEt() {
+
+    }
+
+    @Override
+    public void duetYap() {
+
+    }
+}
+
+class ArabeskSarkici extends Sarkici {
+
+    @Override
+    void sarkiSoyle() {
+        System.out.println("Arabesk sarkici sarki soyluyor");
+
+    }
+
+    @Override
+    public void yemekYe() {
+
+    }
+
+    @Override
+    public void sporYap() {
+
+    }
+}
+
+class PopSarkicisi extends Sarkici {
+
+    @Override
+    void sarkiSoyle() {
+        System.out.println("Pop sarkici sarki soyluyor");
+    }
+
+    @Override
+    public void yemekYe() {
+
+    }
+
+    @Override
+    public void sporYap() {
+
+    }
+}
+
+class MuslumBaba implements ArabeskSarkiciOzellikleri, PopSarkiciOzellikleri {
+
+    @Override
+    public void sahnedeSigaraIc() {
+
+    }
+
+    @Override
+    public void sahnedeDansEt() {
+
+    }
+
+    @Override
+    public void duetYap() {
+
+    }
+}
