aea41f9271f8447d696ad882e719bc31b94f70f5
muhammedazman <vmazman@gmail.com>
2021-02-21 15:25:27 +0100

    StatikDahilSiniflar initialize

---
+++ javaprojects/src/com/company/bolum_10_abstractinterface/static_dahili_siniflar/CerceveSinif.java
@@ -0,0 +1,26 @@
+package com.company.bolum_10_abstractinterface.static_dahili_siniflar;
+
+public class CerceveSinif {
+
+    int nesneDegiskeni;
+    static int staticDegisken;
+
+    public void metot(){
+
+    }
+
+    static void statikMetot(){}
+
+     static class IcSinif{
+        int icSinifNesneDegiskeni;
+        static int icSinifStaticDegisken;
+
+        public void icSinifMetot(){
+            staticDegisken++;
+            statikMetot();
+        }
+
+        static void icSinifStatikMetot(){}
+
+    }
+}
---
+++ javaprojects/src/com/company/bolum_10_abstractinterface/static_dahili_siniflar/StatikDahilSiniflar.java
@@ -0,0 +1,8 @@
+package com.company.bolum_10_abstractinterface.static_dahili_siniflar;
+
+public class StatikDahilSiniflar {
+
+    public static void main(String[] args) {
+
+    }
+}
