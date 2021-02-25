3b251278875caadeb0460370e9af621c3d5c54b4
DESKTOP-J343IEJ\akyus <vmazman@gmail.com>
2021-02-25 19:31:17 +0100

    RasyonelSayi uygulamasi tamamlandi

--- RasyonelSayi/src/Main.java
+++ RasyonelSayi/src/Main.java
@@ -1,19 +1,45 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
-        RasyonelSayi rs1 = new RasyonelSayi(2,4);
+        RasyonelSayi rs1 = new RasyonelSayi(3, 4);
         System.out.println(rs1);
 
-        RasyonelSayi rs2 = new RasyonelSayi(6,-4);
+        RasyonelSayi rs2 = new RasyonelSayi(4, 5);
         System.out.println(rs2);
 
-        RasyonelSayi rs3 = new RasyonelSayi(2,4);
+        RasyonelSayi rs3 = new RasyonelSayi(-2, 5);
         System.out.println(rs3);
 
-        RasyonelSayi rs4 = new RasyonelSayi(3,4);
+        RasyonelSayi rs4 = new RasyonelSayi(8, 9);
         System.out.println(rs4);
 
-        RasyonelSayi rs5 = new RasyonelSayi(-4,-7);
-        System.out.println(rs5);
+
+        RasyonelSayi toplam = rs1.topla(rs2);
+        System.out.println(toplam);
+
+        RasyonelSayi cikar = rs1.cikar(rs2);
+        System.out.println(cikar);
+
+        RasyonelSayi carp = rs1.carp(rs2);
+        System.out.println(carp);
+
+        RasyonelSayi bol = rs1.bol(rs2);
+        System.out.println(bol);
+
+        RasyonelSayi[] sayilar = {rs1, rs2, rs3, rs4};
+        Arrays.sort(sayilar);//Kücükten büyüge
+        for (RasyonelSayi rSayi : sayilar) {
+            System.out.print(rSayi + ", ");
+        }
+
+        System.out.println("");
+
+        Arrays.sort(sayilar, Collections.reverseOrder());//Büyükten kücüge
+        for (RasyonelSayi rSayi : sayilar) {
+            System.out.print(rSayi + ", ");
+        }
     }
 }
--- RasyonelSayi/src/RasyonelSayi.java
+++ RasyonelSayi/src/RasyonelSayi.java
@@ -1,7 +1,12 @@
-public class RasyonelSayi {
+public class RasyonelSayi implements Comparable<RasyonelSayi> {
 
     private int pay;
     private int payda;
+    private String islem;
+    private static String TOPLAM = "Toplam";
+    private static String FARK = "Fark";
+    private static String CARPIM = "Carpim";
+    private static String BOLME = "Bolme";
 
     public RasyonelSayi(){
         pay=0;
@@ -29,12 +34,64 @@ public class RasyonelSayi {
         return ebob;
     }
 
+    public RasyonelSayi topla(RasyonelSayi toplanacakSayi){
+        int yeniPay = (this.pay * toplanacakSayi.payda) + (this.payda * toplanacakSayi.pay);
+        int yeniPayda = payda * toplanacakSayi.payda;
+
+        RasyonelSayi sonuc = new RasyonelSayi(yeniPay, yeniPayda);
+        sonuc.islem = TOPLAM;
+        return sonuc;
+    }
+
+    public RasyonelSayi cikar(RasyonelSayi cikarilacakSayi){
+        int yeniPay = (this.pay * cikarilacakSayi.payda) - (this.payda * cikarilacakSayi.pay);
+        int yeniPayda = payda * cikarilacakSayi.payda;
+
+        RasyonelSayi sonuc = new RasyonelSayi(yeniPay, yeniPayda);
+        sonuc.islem = FARK;
+        return sonuc;
+    }
+
+    public RasyonelSayi carp(RasyonelSayi carpilacakSayi){
+        int yeniPay = this.pay * carpilacakSayi.pay;
+        int yeniPayda = this.payda * carpilacakSayi.payda;
+
+        RasyonelSayi sonuc = new RasyonelSayi(yeniPay, yeniPayda);
+        sonuc.islem =  CARPIM;
+        return sonuc;
+    }
+
+    public RasyonelSayi bol(RasyonelSayi bolunecekSayi){
+        int yeniPay = this.pay * bolunecekSayi.payda;
+        int yeniPayda = this.payda * bolunecekSayi.pay;
+
+        RasyonelSayi sonuc = new RasyonelSayi(yeniPay, yeniPayda);
+        sonuc.islem = BOLME;
+        return sonuc;
+    }
+
+
     @Override
     public String toString() {
+        String sonuc="";
+        if (islem!=null){
+            sonuc=islem + ": ";
+        }
         if(payda==1){
-            return pay + "";
+            return  sonuc + pay + "";
         } else {
-            return pay + "/" + payda;
+            return  sonuc + pay + "/" + payda;
+        }
+    }
+
+    @Override
+    public int compareTo(RasyonelSayi o) {
+        if (this.cikar(o).pay > 0){
+            return 1;
+        } else if (this.cikar(o).pay < 0){
+            return -1;
+        } else {
+            return 0;
         }
     }
 }
