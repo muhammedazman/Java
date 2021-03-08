82a63734ed1af8911d113467ff2ec97c0d0a2406
DESKTOP-J343IEJ\akyus <vmazman@gmail.com>
2021-03-08 20:43:00 +0100

    ilk javafx projesi yazildi

--- FirsJavaFxProject/src/sample/Main.java
+++ FirsJavaFxProject/src/sample/Main.java
@@ -9,15 +9,28 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
+    public void init() throws Exception {
+        System.out.println("init blogu tetiklendi");
+        super.init();
+    }
+
+    @Override
     public void start(Stage primaryStage) throws Exception{
+        System.out.println("start metotu");
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
+    @Override
+    public void stop() throws Exception {
+        System.out.println("stop blogu");
+        super.stop();
+    }
 
     public static void main(String[] args) {
+        System.out.println("main metotu");
         launch(args);
     }
 }
