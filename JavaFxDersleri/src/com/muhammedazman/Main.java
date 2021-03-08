31b9deb50837fe05f5eb82c5a3692e023383213a
DESKTOP-J343IEJ\akyus <vmazman@gmail.com>
2021-03-08 22:35:31 +0100

    JavaFx Dersleri projesi olusturuldu

---
+++ JavaFxDersleri/src/com/muhammedazman/Main.java
@@ -0,0 +1,17 @@
+package com.muhammedazman;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
