d12f4fc7765441daafcdc2b20056aa0476825314
DESKTOP-J343IEJ\akyus <vmazman@gmail.com>
2021-03-10 20:51:20 +0100

    kod icinden ekran tasarlanildi

--- JavaFxDersleri/src/com/muhammedazman/Main.java
+++ JavaFxDersleri/src/com/muhammedazman/Main.java
@@ -1,10 +1,15 @@
 package com.muhammedazman;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -24,10 +29,44 @@ public class Main extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
         BorderPane rootNode = loader.load();
 
+        //Scene Builder dan gelen ekran
         Scene scene = new Scene(rootNode, 600, 400);
         stage.setScene(scene);
         stage.setResizable(false);
         stage.setTitle("Merhaba Java Fx");
+        stage.show();
+
+        //scene1 yukaridaki ekrani ezecektir
+        //stackpane elemanlari tam orta ve üst üste yerlestirir.
+        //StackPane stackPane = new StackPane();
+
+        //elemanlari yan yana dizmek icin
+        HBox hBox = new HBox();
+        Button tamamButton = new Button("Tamam");
+        Button cikisButton = new Button("Cikis");
+        hBox.getChildren().add(tamamButton);
+        hBox.getChildren().add(cikisButton);
+
+
+        //addEventListener
+        tamamButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("tamam tiklanildi");
+            }
+        });
+
+
+        cikisButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("cikis butonu tiklanildi");
+                System.exit(0);
+            }
+        });
+
+        Scene scene1 = new Scene(hBox, 500,400);
+        stage.setScene(scene1);
         stage.show();
     }
 
