152f5a6f0fd06e8cc4df43131c5a053b8d833323
DESKTOP-J343IEJ\akyus <vmazman@gmail.com>
2021-03-16 23:29:51 +0100

    Lambda Comparator, consumer interface, forEach Lambda metotlari gosterildi

--- javaprojects/src/com/company/lambda_ornek/Main.java
+++ javaprojects/src/com/company/lambda_ornek/Main.java
@@ -1,6 +1,9 @@
 package com.company.lambda_ornek;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.function.Consumer;
 
 public class Main {
 
@@ -16,6 +19,7 @@ public class Main {
         calisanArrayList.add(c3);
         calisanArrayList.add(c4);
 
+        // kendi interface sinifimiz ile yapilan ornek
         for (Calisan calisan:calisanArrayList) {
             bilgileriYazdir(new YazdiranInterface() {
                 @Override
@@ -30,15 +34,47 @@ public class Main {
             bilgileriYazdir(() -> System.out.println(calisan.getIsim()+" "+calisan.getYas()));
         }
 
-        new Thread(new ThreadSinifi()).start();
-        new Thread(new Runnable() {
+        // Thread kavarmi
+        //new Thread(new ThreadSinifi()).start();
+       /* new Thread(new Runnable() {
             @Override
             public void run() {
                 System.out.println("Anonim run calisti");
             }
         }).start();
+*/
+      //  new Thread(() -> System.out.println("Lambda anonim calisti")).start();
 
-        new Thread(() -> System.out.println("Lambda anonim calisti")).start();
+        // Comparator
+        Collections.sort(calisanArrayList, new Comparator<Calisan>() {
+            @Override
+            public int compare(Calisan o1, Calisan o2) {
+                return o1.getIsim().compareTo(o2.getIsim());
+            }
+        });
+
+        System.out.println("Comparator ornegi:");
+        for (Calisan calisan:calisanArrayList) {
+            System.out.println(calisan.getIsim());
+        }
+
+        Collections.sort(calisanArrayList,(o1, o2) ->  o2.getIsim().compareTo(o1.getIsim()));
+
+        System.out.println("Comparator Lambda Ornegi:");
+        for (Calisan calisan:calisanArrayList) {
+            System.out.println(calisan.getIsim());
+        }
+
+        // consumer interface
+        calisanArrayList.forEach(new Consumer<Calisan>() {
+            @Override
+            public void accept(Calisan calisan) {
+                System.out.println("Normal kullanim: "+ calisan.getIsim()+" "+calisan.getYas());
+            }
+        });
+
+        // forEach Lambda
+        calisanArrayList.forEach(calisan -> System.out.println("Lambda kullanim: "+ calisan.getIsim()+" "+calisan.getYas()));
     }
 
     public static void bilgileriYazdir(YazdiranInterface yazdiranInterface){
