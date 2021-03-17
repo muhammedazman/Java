6aa707ac38aa58f5f5771fc04c231243edd8044d
DESKTOP-J343IEJ\akyus <vmazman@gmail.com>
2021-03-17 18:22:27 +0100

    DataSource Connect and Close Method

--- MuzikApp/src/com/muhammedazman/Main.java
+++ MuzikApp/src/com/muhammedazman/Main.java
@@ -1,8 +1,12 @@
 package com.muhammedazman;
 
+import com.muhammedazman.model.DataSource;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        DataSource dataSource = new DataSource();
+        dataSource.connectDB();
+        dataSource.closeDB();
     }
 }
---
+++ MuzikApp/src/com/muhammedazman/model/DataSource.java
@@ -0,0 +1,51 @@
+package com.muhammedazman.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// Veritabanlari ile ilgili tum islemleri burdan takip edecegiz
+public class DataSource {
+
+    public static final String DB_NAME = "muzik.db";
+    public static final String CONNECTION_STRING = "jdbc:sqlite:"+DB_NAME;
+
+    // Album Tablosu
+    public static final String TABLO_ALBUM = "album";
+    public static final String SUTUN_ALBUM_ID = "albumID";
+    public static final String SUTUN_ALBUM_ADI = "albumAdi";
+    public static final String SUTUN_ALBUM_SARKICI_ID = "sarkiciID";
+
+    //Sarkici Tablosu
+    public static final String TABLO_SARKICI = "sarkici";
+    public static final String SUTUN_SARKICI_ID = "sarkiciID";
+    public static final String SUTUN_SARKICI_ADI = "sarkiciAdi";
+
+    //Sarki Tablosu
+    public static final String TABLO_SARKI = "sarki";
+    public static final String SUTUN_SARKI_ID = "sarkiID";
+    public static final String SUTUN_SARKI_ADI = "sarkiAdi";
+    public static final String SUTUN_SARKI_ALBUM_ID = "albumID";
+
+    private Connection connection;
+
+    public boolean connectDB() {
+        try {
+            connection = DriverManager.getConnection(CONNECTION_STRING);
+            return true;
+        } catch (SQLException throwables) {
+            System.out.println("Veritabanina baglanilamadi!!!");
+            return false;
+        }
+    }
+
+    public void closeDB() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
