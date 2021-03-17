eb31d1907c3662192c87defb1c93d096115b3aed
DESKTOP-J343IEJ\akyus <vmazman@gmail.com>
2021-03-17 21:48:25 +0100

    Task class setOnSucceed and setOnFailed is writed

--- MuzikAppFXML/src/com/muhammedazman/Controller.java
+++ MuzikAppFXML/src/com/muhammedazman/Controller.java
@@ -1,27 +1,38 @@
 package com.muhammedazman;
 
+import com.jfoenix.controls.JFXProgressBar;
 import com.muhammedazman.model.DataSource;
 import com.muhammedazman.model.Sarkici;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.util.Callback;
-
 public class Controller {
 
     @FXML
     private TableView tablo;
 
     @FXML
-    private TableColumn getirilecekBilgi;
+    private JFXProgressBar progressBar;
 
     public void getAllSingers() {
         GetAllSingers task = new GetAllSingers();
         tablo.itemsProperty().bind(task.valueProperty());
+        progressBar.progressProperty().bind(task.progressProperty());
+        progressBar.setVisible(true);
+
+        task.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+            @Override
+            public void handle(WorkerStateEvent workerStateEvent) {
+                System.out.println("Thread success: "+Thread.currentThread().getName());
+                progressBar.setVisible(false);
+            }
+        });
+
+        task.setOnFailed(event -> progressBar.setVisible(false));
 
         new Thread(task).start();
     }
@@ -33,6 +44,7 @@ class GetAllSingers extends Task {
     @Override
     protected ObservableList<Sarkici> call() throws Exception {
         System.out.println("Thread: "+ Thread.currentThread().getName());
+        Thread.sleep(2000);
 
         // to cast ObservableList from ArrayList
         return FXCollections.observableArrayList(DataSource.getInstance().allSingers(DataSource.ORDER_DESC));
